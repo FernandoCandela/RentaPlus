a0653850e0602fc395ce6983761fa42db21c5783
FernandoCandela <20171937@aloe.ulima.edu.pe>
2021-12-07 11:45:54 -0500

    add edit profile

--- app/src/main/java/pe/edu/ulima/pm/renta/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/renta/MainActivity.kt
@@ -4,17 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -46,11 +46,13 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_profile
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        updateUI()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -76,9 +78,25 @@ class MainActivity : AppCompatActivity() {
             super.onOptionsItemSelected(item)
         }
     }
-    private  fun signOut(){
+
+    private fun signOut() {
         auth.signOut()
         val intent = Intent(this, SignInActivity::class.java)
         this.startActivity(intent)
     }
+
+    private fun updateUI() {
+        val user = auth.currentUser
+        if (user != null) {
+            val navView: NavigationView = binding.navView
+            val headerView = navView.getHeaderView(0)
+            headerView.findViewById<TextView>(R.id.textViewUser).text = user.displayName
+            headerView.findViewById<TextView>(R.id.textViewEmail).text = user.email
+            Glide.with(this).load(user.photoUrl)
+                .placeholder(R.drawable.profile_photo)
+                .into(headerView.findViewById(R.id.imageViewPhoto))
+        }
+    }
+
+
 }
---
+++ app/src/main/java/pe/edu/ulima/pm/renta/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,7 @@
+package pe.edu.ulima.pm.renta.ui.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
